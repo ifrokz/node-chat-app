369f333f215e20159066542f3b301eee6a7093ec
ifrokz <dyfrok@gmail.com>
2018-04-23 19:17:26 +0200

    User experience improved

--- public/index.js
+++ public/index.js
@@ -8,9 +8,21 @@ socket.on('newRoomsList', function (rooms){
     console.log(rooms)
     var list = jQuery('#rooms-list');
     var ul = jQuery('<ul></ul>')
+    
+    list.html('');
+    list.append("<div class='form-field'><h3>Active Rooms</h4></div>");
+
+
     rooms.forEach(function(room) {
         ul.append(jQuery('<li class="room"></li>').text(room));
     });
 
-    list.html(ul)
+    list.append(ul)
+});
+
+$( "body" ).on('click', 'li.room', function(e) {
+    var value = jQuery(e.target).text();
+
+    var input = jQuery('#room');
+    input.val(value)
 });
